58d02ae9a17da18ad50623b2bc541eaf70b55ddf
Alvix.exe <alvixwick@gmail.com>
2024-02-06 17:56:31 -0400

    New lines were added to display the alien ship on the game screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion():
     """Overall class to manage game assets and behavior."""
@@ -20,6 +21,9 @@ class AlienInvasion():
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
         
     def run_game(self):
         """Start the main loop for the game."""
@@ -72,7 +76,13 @@ class AlienInvasion():
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet) 
+                self.bullets.remove(bullet)
+                
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
                 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -80,6 +90,7 @@ class AlienInvasion():
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
         
         pygame.display.flip()
 
