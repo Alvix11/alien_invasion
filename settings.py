bc2a12ef443d2cb2debbb6508ce7e72452db322d
Alvix.exe <alvixwick@gmail.com>
2024-03-22 00:04:11 -0400

    The speed of our ship and bullets was modified.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
         
         # Alien settings
         self.alien_speed = 1.0
@@ -19,7 +19,7 @@ class Settings:
         self.fleet_direction = 1
         
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
