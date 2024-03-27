7c7e66b4aff66a9e36b296302cad0f90cc3807e5
Alvix.exe <alvixwick@gmail.com>
2024-03-26 23:25:50 -0400

    I modified the speed to test the game.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 2.5
+        self.ship_speed = 1.5
         
         # Alien settings
         self.alien_speed = 1.0
