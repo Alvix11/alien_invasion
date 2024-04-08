6f2965a77473540a426f32a25bef46dff7e5cbaf
Alvix.exe <alvixwick@gmail.com>
2024-04-08 18:15:56 -0400

    The prep_Ships () method creates an empty group, self.Ships, to contain the instances of ships, so you believe it in the same way you should read it to see how exactly works.

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard:
     
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -21,6 +22,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
         
     def prep_level(self):
         """Turn the level into a rendered image."""
@@ -32,6 +34,15 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
     
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -44,7 +55,6 @@ class Scoreboard:
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
-    
     
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
